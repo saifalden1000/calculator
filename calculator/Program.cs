6319aca80e24211486738907b0e22a43850ca0d9
Seif Mohamed <seif.abdelsallam1@gmail.com>
2026-09-13 01:23:05 +0300

    feat: initialize calculator project

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the first number: ");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter an operation (+, -, *, /): ");
+string operation = Console.ReadLine();
+
+double result;
+
+switch (operation)
+{
+    case "+":
+        result = firstNumber + secondNumber;
+        break;
+
+    case "-":
+        result = firstNumber - secondNumber;
+        break;
+
+    case "*":
+        result = firstNumber * secondNumber;
+        break;
+
+    case "/":
+        result = firstNumber / secondNumber;
+        break;
+
+    default:
+        Console.WriteLine("Invalid operation.");
+        return;
+}
+
+Console.WriteLine($"Result: {result}");
